d408c76b1e42a9b030e4d9ce54f668cc2ce93226
irmansyah <irmansyahproject@gmail.com>
2025-01-01 01:20:21 +0700

    add support request setup from file

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,31 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, fs::File, io::BufReader};
 
 use clap::{Arg, Command};
 use colored::*;
 use reqwest::blocking::Client;
+use serde::Deserialize;
 use serde_json::Value;
+
+#[derive(Debug, Deserialize)]
+struct AppMainRequest {
+    app_title: String,
+    base_url: String,
+    requests: Vec<RequestData>,
+}
+
+#[derive(Clone, Debug, Deserialize)]
+struct RequestData {
+    req_title: String,
+    req_type: String,
+    req_end_point: String,
+    req_body: Option<RequestDataBody>,
+}
+
+#[derive(Clone, Debug, Deserialize)]
+struct RequestDataBody {
+    body_type: String,
+    body_file: String,
+}
 
 fn display_colored_json(value: &Value, indent_level: usize) {
     match value {
@@ -46,100 +68,107 @@ fn display_colored_json(value: &Value, indent_level: usize) {
 fn main() {
     let matches = Command::new("API CLI")
         .version("1.0")
-        .author("Your Name <you@example.com>")
+        .author("Irmansyah <irmansyahproject@gmail.com>")
         .about("CLI app for making API requests")
         .arg(
-            Arg::new("url")
-                .short('u')
-                .long("url")
-                .value_name("URL")
-                .help("The API endpoint URL")
+            Arg::new("file")
+                .short('f')
+                .long("file")
+                .value_name("FILE")
+                .help("The API FILE")
                 .required(true),
-        )
-        .arg(
-            Arg::new("method")
-                .short('m')
-                .long("method")
-                .value_name("METHOD")
-                .help("HTTP method (GET, POST, etc.)")
-                .default_value("GET"),
-        )
-        .arg(
-            Arg::new("data")
-                .short('d')
-                .long("data")
-                .value_name("DATA")
-                .help("JSON payload for POST/PUT requests"),
         )
         .get_matches();
 
-    // Extract arguments
-    let url = matches.get_one::<String>("url").unwrap();
-    let method = matches.get_one::<String>("method").unwrap().to_uppercase();
-    let data = matches.get_one::<String>("data");
+    let file = matches.get_one::<String>("file");
 
     // Initialize HTTP client
     let client = Client::new();
 
-    // Make the request
-    let response = match method.as_str() {
-        "GET" => client.get(url).send(),
-        "POST" => {
-            // Check if the data is form data or raw data
-            if let Some(form_data) = data {
-                // If the data looks like form data (contains '&' and '='), treat it as form data
-                if form_data.contains('&') && form_data.contains('=') {
-                    let mut form = HashMap::new();
-                    for pair in form_data.split('&') {
-                        let mut parts = pair.splitn(2, '=');
-                        if let (Some(key), Some(value)) = (parts.next(), parts.next()) {
-                            form.insert(key.to_string(), value.to_string());
-                        }
-                    }
-                    client.post(url).form(&form).send()
-                } else {
-                    // Otherwise treat it as raw data (likely JSON or plain text)
-                    client.post(url).body(form_data.to_string()).send()
-                }
-            } else {
-                // If no data is provided, just send an empty POST request
-                client.post(url).send()
-            }
-        }
-        "PUT" => client
-            .put(url)
-            .body(data.map_or("", |v| v).to_string())
-            .send(),
-        "DELETE" => client.delete(url).send(),
-        _ => {
-            eprintln!("{}", "Unsupported HTTP method".red());
-            return;
-        }
-    };
+    if let Some(file_data) = file {
+        let file = File::open(file_data).expect("File should open read only");
+        let reader = BufReader::new(file);
+        let app_main_request: AppMainRequest =
+            serde_json::from_reader(reader).expect("File should be proper JSON");
 
-    // Handle response
-    match response {
-        Ok(resp) => {
-            let status = resp.status();
-            let body: Value = resp.json().unwrap_or_else(
-                |_| serde_json::json!({"error": "Failed to parse response as JSON"}),
-            );
-            println!("");
-            println!("");
-            println!(
-                "{} {}",
-                "Status   :".blue().bold(),
-                status.to_string().green()
-            );
-            println!("{}", "Response :".blue().bold());
-            println!("");
-            display_colored_json(&body, 0); // Display formatted and colored JSON
-            println!("");
-            println!("");
-        }
-        Err(err) => {
-            eprintln!("{}", "Error:".red().bold());
-            eprintln!("{}", err.to_string().red());
+        let request = app_main_request.requests.get(1).unwrap();
+
+        let url = app_main_request.base_url + &request.req_end_point;
+        println!("url : {}", url.to_string());
+        let method = &request.req_type;
+
+        // Make the request
+        let response = match method.as_str() {
+            "GET" => client.get(url).send(),
+            "POST" => match &request.req_body {
+                Some(req_body) => {
+                    let body_file_path = req_body.body_file.clone();
+                    println!("body_file_path : {}", body_file_path);
+                    let body_file = File::open(body_file_path).expect("Failed to read file");
+                    let body_reader = BufReader::new(body_file);
+                    let body_data: HashMap<String, Value> =
+                        serde_json::from_reader(body_reader).expect("File should be proper JSON");
+                    let pretty_json_string = serde_json::to_string_pretty(&body_data)
+                        .expect("Failed to convert to pretty JSON string");
+                    println!("request.req_type : {:?}", request.req_type);
+                    if req_body.body_type == "FORM_DATA" {
+                        client.post(url).form(&body_data).send()
+                    } else {
+                        client.post(url).body(pretty_json_string).send()
+                    }
+                }
+                None => client.post(url).send(),
+            },
+            "PUT" => match &request.req_body {
+                Some(req_body) => {
+                    let body_file_path = req_body.body_file.clone();
+                    println!("body_file_path : {}", body_file_path);
+                    let body_file = File::open(body_file_path).expect("Failed to read file");
+                    let body_reader = BufReader::new(body_file);
+                    let body_data: HashMap<String, Value> =
+                        serde_json::from_reader(body_reader).expect("File should be proper JSON");
+                    let pretty_json_string = serde_json::to_string_pretty(&body_data)
+                        .expect("Failed to convert to pretty JSON string");
+                    println!("request.req_type : {:?}", request.req_type);
+                    if req_body.body_type == "FORM_DATA" {
+                        client.put(url).form(&body_data).send()
+                    } else {
+                        client.put(url).body(pretty_json_string).send()
+                    }
+                }
+                None => client.post(url).send(),
+            },
+            "DELETE" => client.delete(url).send(),
+            _ => {
+                eprintln!("{}", "Unsupported HTTP method".red());
+                return;
+            }
+        };
+
+        // Handle response
+        match response {
+            Ok(resp) => {
+                let status = resp.status();
+                let body: Value = resp.json().unwrap_or_else(
+                    |_| serde_json::json!({"error": "Failed to parse response as JSON"}),
+                );
+                println!("");
+                println!("");
+                println!(
+                    "{} {}",
+                    "Status   :".blue().bold(),
+                    status.to_string().green()
+                );
+                println!("{}", "Response :".blue().bold());
+                println!("");
+                display_colored_json(&body, 0); // Display formatted and colored JSON
+                println!("");
+                println!("");
+            }
+            Err(err) => {
+                eprintln!("{}", "Error:".red().bold());
+                eprintln!("{}", err.to_string().red());
+            }
         }
     }
 }
